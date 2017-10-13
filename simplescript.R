8566f29d9aed103809d92bc0d5239c38bede6e40
yangslmn <32276669+yangslmn@users.noreply.github.com>
2017-10-13 14:15:48 -0500

    add name

--- simplescript.R
+++ simplescript.R
@@ -10,13 +10,13 @@ x <- (-(sampleSize-1)/2):((sampleSize-1)/2)
 # create dependent values according to ax + b + N(0,sd)... similar to linear regression model
 y <-  trueA * x + trueB + rnorm(n=sampleSize,mean=0,sd=trueSd) #vector y
 
-plot(x,y, main="Test Data")
+plot(x,y, main="Test Data") # make plot
 
 
 # Example: plot the likelihood profile of the slope a
 
 slopelikelihoods <- lapply(seq(3, 7, by=.05), slopevalues ) # apply the slopevalues function to each list.
-plot (seq(3, 7, by=.05), slopelikelihoods , type="l", xlab = "values of slope parameter a", ylab = "Log likelihood")
+plot (seq(3, 7, by=.05), slopelikelihoods , type="l", xlab = "values of slope parameter a", ylab = "Log likelihood") # make the plot about likelihood
 
 
 compare_outcomes<-function(iterations)
